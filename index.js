3b25f651b12724cee16462cb07aabf06efb86d9a
Soumyanil Das <dassic@outlook.com>
2023-06-19 02:50:38 +0530

    added join room and message snd

--- index.js
+++ index.js
@@ -23,13 +23,29 @@ const chat_rooms = [
     { name: "chess", creator: "anon" }
 ]
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
     socket.on("create_user", (username) => {
         socket.username = username;
         usernames[username] = username;
         socket.current_room = "global_chat";
+        // console.log(socket);
+        socket.join("global_chat");
+        socket.emit("update_chat", "INFO", "You have joined Global Chat");
+    });
 
-        console.log(socket);
+    socket.on("send_message", (data) => {
+        io.sockets.to(socket.current_room).emit("update_chat", socket.username, data);
+    });
+
+    socket.on("update_rooms", (room) => {
+        socket.broadcast
+            .to(socket.currentRoom)
+            .emit("updateChat", "INFO", socket.username + " left room");
+        socket.leave(socket.currentRoom);
+        socket.current_room = room;
+        socket.join(room);
+        socket.emit("update_chat", "INFO", "You have joined " + room + " room");
+        socket.broadcast.to(room).emit("update_chat", "INFO", socket.username + " has joined " + room + " room");
     });
 })
 
--- public/app.js
+++ public/app.js
@@ -14,4 +14,37 @@ var my_username = "";
 socket.on("connect", () => {
     my_username = prompt("Enter Name :");
     socket.emit("create_user", my_username);    // this my_username argument will be received in the backend at the argument of the callback function of this connection
-})
+});
+
+send_message_btn.addEventListener('click', () => {
+    socket.emit('send_message', message.value);
+    message.value = "";
+});
+
+socket.on("update_chat", (type, data) => {
+    if (type === "INFO") {
+        chat_display.innerHTML = `<div class='announcement'><span>${data}</span></div>`;
+    } else {
+
+        chat_display.innerHTML += `<div class="message_holder ${type === my_username ? "me" : ""}">
+                                    <div class="pic"></div>
+                                        <div class="message_box">
+                                            <div id="message" class="message">
+                                                <span class="message_name">${type}</span>
+                                                <span class="message_text">${data}</span>
+                                            </div>
+                                    </div>
+                                </div>`;
+    }
+
+    chat_display.scrollTop = chat_display.scrollHeight;
+});
+
+function changeRoom(room) {
+    if (room != currentRoom) {
+        socket.emit("updateRooms", room);
+        document.getElementById(currentRoom).classList.remove("active_item");
+        currentRoom = room;
+        document.getElementById(currentRoom).classList.add("active_item");
+    }
+}
